c1f941f71fd9821b028fd5257a748bdf2bc530b1
joel <joel.uss1728@gmail.com>
2022-07-02 14:33:06 -0500

    Se crea el archivo .env
    mantiene de forma segura los parametros de la conexion a la base  de datos

--- Database.php
+++ Database.php
@@ -1,25 +1,44 @@
 <?php
 class Database 
+
+//
+
+
+//
+
 {
+
+  
 /*
   CODIGO DE LA CONEXION A LA BASE DE DATOS
 */
-private static $dbName = 'prueba';  // el nombre de la base de  datos 
-private static $dbHost = 'localhost'; 
-private static $dbUsername = 'root'; 
-private static $dbUserPassword = 'root'; // poner su contraseña
+private static $dbName;// = 'prueba';// = 'prueba';  // el nombre de la base de  datos 
+private static $dbHost ;//= 'localhost'; 
+private static $dbUsername;//= 'root'; 
+private static $dbUserPassword;//= 'root'; // poner su contraseña
 
 private static $cont = null;
 
+
 public function __construct() {
-die('Init-Función no permitida');
+  die('Init-Función no permitida');
 }
 
 public static function connect() {
-// Permitir solo una conexión para la totalidad del acceso
-if ( null == self::$cont )
-{
+  // Permitir solo una conexión para la totalidad del acceso
+  if ( null == self::$cont )
+
+  {
   try{
+    $contenido = parse_ini_file('.env', false);
+    self::$dbName = $contenido['DB_NAME']; 
+    self::$dbHost = $contenido['DB_HOST'];
+    self::$dbUsername = $contenido['DB_USER'];
+    self::$dbUserPassword = $contenido['DB_PASSWD'];
+
+    
+    // include_once 'loadEnv.php';
+    
      self::$cont = new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword);
   }
   catch(PDOException $e){
